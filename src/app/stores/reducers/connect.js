9f15a8e48ab6ae8de12114a526094be2d6904050
Danil <raymondcostner@gmail.com>
2020-09-21 09:53:53 +0300

    feat: add base stores

---
+++ src/app/stores/reducers/connect.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const connectSlice = createSlice({
+  name: 'connect',
+  initialState: {},
+  reducers: {},
+});
+
+export default connectSlice.reducer;
